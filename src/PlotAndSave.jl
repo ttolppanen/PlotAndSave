1eeef04c27ede6cf275be436c5fb8c481e519956
ttolppanen <taneli.tolppanen@gmail.com>
2023-03-16 16:05:51 +0200

    added trajectories

--- src/PlotAndSave.jl
+++ src/PlotAndSave.jl
@@ -5,16 +5,17 @@ using Gadfly
 
 include("PlotStructs.jl")
 
-export Plot
+export makeplot
+export addtrajectories
 
-function Plot(path, lines...; xlabel::String = "x", ylabel::String = "y", param...)
+function makeplot(path, lines...; xlabel::String = "x", ylabel::String = "y", param...)
     plotinfo = PlotInfo(lines...; xlabel, ylabel, param...)
     mkpath(path)
-    SavePlot(path, plotinfo)
+    saveplot(path, plotinfo)
     jldsave(joinpath(path, "data.jld2"); plotinfo)
 end
 
-function SavePlot(path::String, plotinfo::PlotInfo)
+function saveplot(path::String, plotinfo::PlotInfo)
     layers = []
     for lineinfo in values(plotinfo.lines)
         new_layer = layer(x=lineinfo.x, y=lineinfo.y, Geom.line, color = [lineinfo.tag])
@@ -22,6 +23,22 @@ function SavePlot(path::String, plotinfo::PlotInfo)
     end
     p = plot(layers..., Guide.title(plotinfo.title), Guide.xlabel(plotinfo.xlabel), Guide.ylabel(plotinfo.ylabel))
     p |> SVG(joinpath(path, "plot.svg"))
+end
+
+function addtrajectories(path_to_prev_data::String, new_trajectories...)
+    plotinfo = load(joinpath(path_to_prev_data, "data.jld2"), "plotinfo")
+    new_lines = plotinfo.lines
+    for traj in new_trajectories
+        old_line = plotinfo.lines[traj.tag]
+        new_lines[traj.tag] = updateline(old_line, traj)
+    end
+    makeplot(path_to_prev_data, values(new_lines)...; xlabel = plotinfo.xlabel, ylabel = plotinfo.ylabel, plotinfo.parameters...)
+end
+
+function updateline(old_line::LineInfo, new_line::LineInfo)
+    total_traj = old_line.traj + new_line.traj
+    x = (old_line.traj .* old_line.x .+ new_line.traj .* new_line.x) ./ total_traj
+    return LineInfo(x, new_line.y, total_traj, new_line.tag)
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,38 @@ using JLD2
 
 @testset "Plot and Save" begin
 
-x1 = rand(10); y1 = rand(10)
-x2 = rand(10); y2 = rand(10)
-line1 = LineInfo(x1, y1, 9, "1")
-line2 = LineInfo(x2, y2, 1, "2")
-path = joinpath(@__DIR__, "test_data")
-Plot(path, line1, line2; ylabel = "Random Values", dt = 0.2, n = 4, testing = "abcd")
+line1_traj = 9
+line2_traj = 1
 
-plotinfo = jldopen(joinpath(path, "data.jld2"))["plotinfo"]
-@test plotinfo.lines["1"].x == x1
-@test plotinfo.lines["2"].x == x2
-@test plotinfo.lines["1"].y == y1
-@test plotinfo.lines["2"].y == y2
-@test plotinfo.lines["1"].traj == 9
+    @testset "Saving" begin
+        x1 = rand(10); y1 = rand(10)
+        x2 = rand(10); y2 = rand(10)
+        line1 = LineInfo(x1, y1, line1_traj, "1")
+        line2 = LineInfo(x2, y2, line2_traj, "2")
+        path = joinpath(@__DIR__, "test_data")
+        makeplot(path, line1, line2; ylabel = "Random Values", dt = 0.2, n = 4, testing = "abcd")
+
+        plotinfo = load(joinpath(path, "data.jld2"), "plotinfo")
+        @test plotinfo.lines["1"].x == x1
+        @test plotinfo.lines["2"].x == x2
+        @test plotinfo.lines["1"].y == y1
+        @test plotinfo.lines["2"].y == y2
+        @test plotinfo.lines["1"].traj == line1_traj
+    end
+
+    @testset "Updating Trajectories" begin
+        x1 = rand(10); y1 = rand(10)
+        x2 = rand(10); y2 = rand(10)
+        line1 = LineInfo(x1, y1, line1_traj, "1")
+        line2 = LineInfo(x2, y2, line2_traj, "2")
+        path = joinpath(@__DIR__, "test_data")
+        addtrajectories(path, line1, line2)
+
+        plotinfo = load(joinpath(path, "data.jld2"), "plotinfo")
+        @test plotinfo.lines["1"].y == y1
+        @test plotinfo.lines["2"].y == y2
+        @test plotinfo.lines["1"].traj == line1_traj * 2
+        @test plotinfo.lines["2"].traj == line2_traj * 2
+    end
 
 end # testset
